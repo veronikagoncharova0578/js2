6e56d6ae764d7fa67dad2c898e965a6b1f7976dd
veronikagoncharova0578 <57313917+veronikagoncharova0578@users.noreply.github.com>
2020-03-08 13:59:45 +0200

    js 2

--- js/task3.js
+++ js/task3.js
@@ -1,1 +1,2 @@
 const random = Math.random() * (1 - 10) + 1;
+console.log(Math.pow(random, 2));
---
+++ js/task4.js
@@ -0,0 +1,5 @@
+const w1 = "My";
+const w2 = "name";
+const w3 = "is";
+const fullName = w1 + w2 + w3 + "Viktor";
+console.log(fullName);
